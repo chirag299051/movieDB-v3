6ad5aaa24f0effc7951c02b429b12fdf107a2d7a
Chirag Singh <chirag@Chirags-MacBook-Pro.local>
2023-08-19 15:57:10 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.listen(API_PORT, () => {
   console.log(`Server is listening on port ${API_PORT}`);
 });
 
-const buildPath = path.join(__dirname, "../app/build/movieDB-v3");
+const buildPath = path.join(__dirname, "../app/build");
 
 app.use(express.static(buildPath));
 
