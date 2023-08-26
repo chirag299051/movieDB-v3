019e3caae25a501b9fa4cf74af5d6c902845b33a
Chirag Singh <chirag@Chirags-MacBook-Pro.local>
2023-08-26 22:43:54 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const app = express();
 const authRoute = require("./Routes/AuthRoutes");
 const userRoute = require("./Routes/UserRoutes");
 require("dotenv").config();
-const bodyParser = require("body-parser");
 const path = require("path");
 
 const MONGODB_URL = process.env.MONGODB_URL;
@@ -20,23 +19,12 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use(cors());
-app.options("*", cors()); // this enables preflight
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
 const buildPath = path.join(__dirname, "../app/build");
-
 app.use(express.static(buildPath));
-
-// app.use(
-//   cors({
-//     origin: ["https://moviedb-v3.onrender.com", "http://localhost:3000"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json());
 
 app.use("/", authRoute);
 app.use("/", userRoute);
