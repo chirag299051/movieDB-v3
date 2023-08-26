0f96599a40502bc25f101307b5b3e1bc5fa5e052
Chirag Singh <chirag@Chirags-MacBook-Pro.local>
2023-08-26 17:13:30 +0530

    updates

--- app/src/shared/Loading.js
+++ app/src/shared/Loading.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loading = () => {
-  return <div className="loading">Signing in...</div>;
+  return <div className="loading"></div>;
 };
 
 export default Loading;
